2ba727c9b2d18eb31556a5cdf27e88e1c541c38f
Ileana P <ilepetrisor@gmail.com>
2022-12-04 11:26:55 +0200

    Day 4 puzzle

---
+++ day4/day4_2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cp input.backup input
+sed -i 's/-/,/g' input
+
+filename='input'
+
+overlapping=0
+limits=()
+
+while read -r line
+do	
+	limits=($(echo $line | tr "," " "))
+	l11=${limits[0]}
+	l12=${limits[1]}
+	l21=${limits[2]}
+	l22=${limits[3]}
+	if [[ $l21 -ge $l11 ]] && [[ $l21 -le $l12 ]] && [[ $l22 -ge $l11 ]] && [[ $l22 -le $l12 ]]
+        then
+        	overlapping=$((overlapping+1))
+        else
+        	if [[ $l11 -ge $l21 ]] && [[ $l11 -le $l22 ]] && [[ $l12 -ge $l21 ]] && [[ $l12 -le $l22 ]]
+        	then
+                	overlapping=$((overlapping+1))
+                else
+			if [[ $l21 -le $l11 ]] && [[ $l22 -le $l12 ]] && [[ $l22 -ge $l11 ]] 
+			then
+				overlapping=$((overlapping+1))
+			else
+				if [[ $l21 -ge $l11 ]] && [[ $l22 -ge $l12 ]] && [[ $l21 -le $l12 ]]
+				then
+		                	overlapping=$((overlapping+1))
+       				fi
+			fi
+		fi
+	fi
+done < $filename
+echo "Overlapping pairs: $overlapping"
